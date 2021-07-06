91ed7864f7680ad5b1867146192640d1519394b6
Elliot_Connell <elliotconnell@gmail.com>
2021-07-06 15:42:04 +1000

    submitted version of test suite

--- src/test/java/DemoTest.java
+++ src/test/java/DemoTest.java
@@ -4,7 +4,9 @@
  * {@link Demo#main(String[])} and 
  * {@link Demo#isTriangle(double, double, double)}).
  */
-//import java.io.*;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
@@ -25,5 +27,68 @@ public class DemoTest {
 	public void test_is_triangle_3() {
 		assertTrue(Demo.isTriangle(5, 13, 12));		
 	}
+	
+	@Test
+	public void test_is_triangle_4() {
+		assertTrue(Demo.isTriangle(12, 5, 13));
+	}
+	
+	@Test
+	public void test_is_triangle_5() {
+		assertTrue(Demo.isTriangle(12, 13, 5));
+		
+	}
+	
+	@Test
+	public void test_is_NOT_triangle_1() {
+		assertFalse(Demo.isTriangle(5, 7, 13));
+	}
+	
+	@Test
+	public void test_is_NOT_triangle_2() {
+		assertFalse(Demo.isTriangle(5, 13, 7));
+	}
+	
+	@Test
+	public void test_is_NOT_triangle_3() {
+		assertFalse(Demo.isTriangle(13, 5, 7));
+	}
+	
+	@Test
+	public void test_is_NOT_triangle_4() {
+		assertFalse(Demo.isTriangle(13, 7, 5));
+	}
+	
+	@Test
+	public void  test_is_NOT_triangle_5() {
+		assertFalse(Demo.isTriangle(13, 7, 5));
+	}
+	
+	@Test
+	public void test_is_NOT_triangle_6() {
+		assertFalse(Demo.isTriangle(5, 9, 3));
+	}
+	
+	@Test
+	public void test_is_NOT_triangle_7() {
+		assertFalse(Demo.isTriangle(1, 2, -3));
+	}
+	/*
+	@Test
+	public void test_main_program_1() {
+		ByteArrayInputStream in = new ByteArrayInputStream("5\n12\n13\n".getBytes());
+		System.setIn(in);
+		
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(out));
+		
+		String consoleOutput = "Enter side 1: \n";
+		consoleOutput += "Enter side 2: \n";
+		consoleOutput += "Enter side 3: \n";
+		consoleOutput += "This is a triangle.\n";
+		
+		assertEquals(consoleOutput, out.toString());
+	}
+	*/
 
 }
