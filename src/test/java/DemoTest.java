825dbe65443a8ce542fede526772d41c036700b0
ElliotConnell <elliot.connell@gmail.com>
2021-07-08 12:54:48 +1000

    uncommented import statement

--- src/test/java/DemoTest.java
+++ src/test/java/DemoTest.java
@@ -6,7 +6,7 @@
  */
 //import java.io.ByteArrayInputStream;
 //import java.io.ByteArrayOutputStream;
-//import java.io.PrintStream;
+import java.io.PrintStream;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
